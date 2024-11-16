69f7149f702abbaf40c40701ae5353fa4e0fbcf4
MariaFernandaFernandezAlva <137630833+MariaFernandaFernandezAlva@users.noreply.github.com>
2024-11-15 22:10:48 -0500

    style: removal of unnecessary comments

--- src/contexts/rentals/services/tenant-api.services.js
+++ src/contexts/rentals/services/tenant-api.services.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const API_URL = 'http://localhost:3000/';
 
 export class TenantApiServices {
-  // Obtener todos los usuarios
   async getAllUsers() {
     return await axios.get(`${API_URL}User`);
   }
@@ -23,14 +22,12 @@ export class TenantApiServices {
   async createTenant(tenantData) {
     return await axios.post(`${API_URL}tenants`, tenantData);
   }
-  // Actualizar al inquilino con el nuevo request
   async updateTenantRequest(tenantId, requestData) {
     return await axios.put(`${API_URL}tenants/${tenantId}`, requestData);
   }
   async updateTenantDates(requestId, updatedData) {
     return await axios.patch(`${API_URL}requests/${requestId}`, updatedData);
   }
-  // Obtener todos los request
   async getAllRequests() {
     return await axios.get(`${API_URL}requests`);
   }
@@ -40,22 +37,18 @@ export class TenantApiServices {
   async createRequest(requestData) {
     return await axios.post(`${API_URL}requests`, requestData);
   }
-  // Obtener todos los propietarios
   async getAllOwners() {
     return await axios.get(`${API_URL}owners`);
   }
-  // Actualizar el propietario con el nuevo vehículo
   async updateOwnerVehicles(ownerId, vehicleData) {
     return await axios.put(`${API_URL}owners/${ownerId}`, vehicleData);
   }
-  // Obtener todos los vehículos
   async getAllVehicles() {
     return await axios.get(`${API_URL}vehicles`);
   }
   async getVehicleById(vehicleId) {
     return await axios.get(`${API_URL}vehicles/${vehicleId}`);
   }
-  // Eliminar un vehículo específico de un propietario
   async deleteVehicle(vehicleId) {
     return await axios.delete(`${API_URL}vehicles/${vehicleId}`);
   }
@@ -65,7 +58,6 @@ export class TenantApiServices {
   async createVehicle(vehicleData) {
     return await axios.post(`${API_URL}vehicles`, vehicleData);
   }
-  // Registrar un nuevo usuario
   async registerUser(user) {
     return await axios.post(`${API_URL}User`, user);
   }
