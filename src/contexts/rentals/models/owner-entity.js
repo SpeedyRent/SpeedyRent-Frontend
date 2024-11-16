43017de00c4d43ba59c304c652188e341e11c24c
MariaFernandaFernandezAlva <137630833+MariaFernandaFernandezAlva@users.noreply.github.com>
2024-11-15 22:09:56 -0500

    style: removal of unnecessary comments

--- src/contexts/rentals/models/owner-entity.js
+++ src/contexts/rentals/models/owner-entity.js
@@ -6,6 +6,6 @@ export class OwnerEntity {
   ) {
     this.id = id !== null ? id : '';
     this.owner_id = owner_id;
-    this.vehicles = vehicles.length ? vehicles : [];  // Garantiza un arreglo vacío si no se pasa nada
+    this.vehicles = vehicles.length ? vehicles : [];
   }
 }
